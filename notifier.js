4413f9eed54b33bce35ac5242e395fab963ffd1e
manoj m bharadwaj <ibeingmj@gmail.com>
2015-12-10 19:27:56 +0530

    can have multiple notifiers
    enjoy multiple notifiers

--- notifier.js
+++ notifier.js
@@ -1,23 +1,29 @@
 /**
  * Simple jQuery Notification Messages.
  * @param  Notofication Text, Notification Class
- * @param  					  Default: success,
- *                			  all classes : success, error, info
- * @author Manoj TM (ibeingmj@gmail.com)
+ * @param  Default: success,
+ *         all classes : success, error, info
+ * 
  */
 jQuery(document).ready(function () {
 	jQuery.fn.notify = function(notificationText, notificationType='success') {
 		var notificationClassName = 'notifier notifier-'+notificationType;
-		var div = '<div onclick="hideThis(this)" id="notify-main" class="'+notificationClassName+'">'+ notificationText +'</div>';
+		var styleHeight = '';
+		if ($('.notifier').length > 0) {
+			/** calculate bottom height if any notification exists, not prevent overlaping */
+			var styleHeight = 'margin-bottom: '+($('.notifier').length*70)+'px';
+		}
+		var div = '<div onclick="hideThis(this)" id="notify-main" style="'+styleHeight+'" class="'+notificationClassName+'">'+ notificationText +'</div>';
 		jQuery('body').append(div);
 	}
 
 	setTimeout(function () {
-		$('.notifier').fadeOut('slow');
+		$('.notifier').remove().fadeOut('slow');
 	}, 5000);
-	// $('#notify-main').click(function() { $(this).fadeOut('fast') });
+
+	
 	hideThis = function ($this)
 	{
-		$($this).fadeOut('fast');
+		$($this).remove().fadeOut('slow');
 	}
 });
